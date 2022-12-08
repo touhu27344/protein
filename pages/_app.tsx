38a04744785b48862edf9af7a1196022fe11553d
touhu27344 <tk27344@icloud.com>
2022-12-08 23:27:44 +0900

    AgendaHubを作りました

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import type { AppProps } from 'next/app'
 import Header from "../components/header";
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <div>
-        <Header
-            pageName='ぼけ'
-        />
-        <Component {...pageProps} />
-      </div>
+    <ChakraProvider>
+      <Header pageName='ぼけ' />
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
+
+export default MyApp
---
+++ pages/login.tsx
@@ -0,0 +1,104 @@
+import { Box, Button, Card, CardBody, Center, FormControl, FormLabel, Heading, HStack, Input, Text, Link, Stack, VStack } from "@chakra-ui/react";
+import { GithubLogo } from "../components/logo/gitHubLogo";
+
+const Login = () => {
+  return (
+    <Box>
+      <Center>
+        <Stack spacing='4'>
+          <VStack as='header' spacing='6' mt='8'>
+            <GithubLogo />
+            <Heading
+              as='h1'
+              fontWeight='300'
+              fontSize='24px'
+              letterSpacing='-0.5px'
+            >
+              Sign in to AgendaHub
+            </Heading>
+          </VStack>
+          <Card bg='#f6f8fa' variant='outline' borderColor='#d8dee4' w='308px'>
+            <CardBody>
+              <form>
+                <Stack spacing='4'>
+                  <FormControl>
+                    <FormLabel size='sm'>Your mentor's name</FormLabel>
+                    <Input
+                      type='text'
+                      bg='white'
+                      borderColor='#d8dee4'
+                      size='sm'
+                      borderRadius='6px'
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <HStack justify='space-between'>
+                      <FormLabel size='sm'>Your favorite words</FormLabel>
+                      <Button
+                        as='a'
+                        href='#'
+                        variant='link'
+                        size='xs'
+                        color='#0969da'
+                        fontWeight='500'
+                      >
+                        Forgot one?
+                      </Button>
+                    </HStack>
+                    <Input
+                      type='password'
+                      bg='white'
+                      borderColor='#d8dee4'
+                      size='sm'
+                      borderRadius='6px'
+                    />
+                  </FormControl>
+
+                  <Button
+                    bg='#2da44e'
+                    color='white'
+                    size='sm'
+                    _hover={{ bg: '#2c974b' }}
+                    _active={{ bg: '#298e46' }}
+                  >
+                    Sign in
+                  </Button>
+                </Stack>
+              </form>
+            </CardBody>
+          </Card>
+
+          <Card variant='outline' borderColor='#d0d7de'>
+            <CardBody>
+              <Center>
+                <HStack fontSize='sm' spacing='1'>
+                  <Text>You Don't have mentor?</Text>
+                  <Link isExternal color='#0969da' href='#'>
+                    Create an account.
+                  </Link>
+                </HStack>
+              </Center>
+            </CardBody>
+          </Card>
+        </Stack>
+      </Center>
+      <Center as='footer' mt='16'>
+        <HStack spacing='4' pt='2'>
+          <Link isExternal color='#0969da' href='#' fontSize='xs'>
+            Terms
+          </Link>
+          <Link isExternal color='#0969da' href='#' fontSize='xs'>
+            Privacy
+          </Link>
+          <Link isExternal color='#0969da' href='#' fontSize='xs'>
+            Security
+          </Link>
+          <Link isExternal href='#' fontSize='xs'>
+            Contact AgendaHub
+          </Link>
+        </HStack>
+      </Center>
+    </Box>
+  )
+}
+export default Login;
